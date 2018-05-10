63da80c1d1636df20f01e1ce07bb783c434afaa6
greg-sk <grzegorz.luczywo@securitykiss.com>
2018-05-10 14:55:36 +0200

    work in progress

--- durablemap2.go
+++ durablemap2.go
@@ -34,3 +34,18 @@ func (o *DurableMap2) Get(k interface{}, resolve func() (interface{}, error)) (v
 		return v, nil
 	}
 }
+
+func (o *DurableMap2) GetInt(k interface{}, resolve func() (interface{}, error)) (v int, err error) {
+	temp, err := o.Get(k, resolve)
+	return temp.(int), err
+}
+
+func (o *DurableMap2) GetInt64(k interface{}, resolve func() (interface{}, error)) (v int64, err error) {
+	temp, err := o.Get(k, resolve)
+	return temp.(int64), err
+}
+
+func (o *DurableMap2) GetString(k interface{}, resolve func() (interface{}, error)) (v string, err error) {
+	temp, err := o.Get(k, resolve)
+	return temp.(string), err
+}
--- durablemap3.go
+++ durablemap3.go
@@ -24,3 +24,18 @@ func (o *DurableMap3) Get(k interface{}) (v interface{}, err error) {
 		return nil, nil
 	}
 }
+
+func (o *DurableMap3) GetInt(k interface{}) (v int, err error) {
+	temp, err := o.Get(k)
+	return temp.(int), err
+}
+
+func (o *DurableMap3) GetInt64(k interface{}) (v int64, err error) {
+	temp, err := o.Get(k)
+	return temp.(int64), err
+}
+
+func (o *DurableMap3) GetString(k interface{}) (v string, err error) {
+	temp, err := o.Get(k)
+	return temp.(string), err
+}
